290a7b4900b41640af62efda763fa6213a37b315
Aidan Kiely <124akiely@frhsd.com>
2024-02-28 08:31:53 -0500

    .

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'They are socks',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            inStock: true
+            // solution 
+        }
+    }
+})
